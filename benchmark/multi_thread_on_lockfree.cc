7bfa3c7ac4d1275df4430de42553794a471a8be9
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-09-07 18:41:11 +0800

    adopt a debug methods

--- benchmark/multi_thread_on_lockfree.cc
+++ benchmark/multi_thread_on_lockfree.cc
@@ -1,6 +1,7 @@
 #include "benchmark_workers.h"
 #include "queue_lru.h"
 #include <mutex>
+#include <set>
 
 namespace t = ::std::chrono;
 namespace r = ::std::ranges;
@@ -22,11 +23,21 @@ static queue_lru<key_t, value_t> cache(benchmark_cache_size());
     const auto tp = tic();
     [[maybe_unused]] size_t hits{}, misses{};
 
+    ::std::set<key_t> debug_set;
+
     size_t iteration{};
     for (auto k : gen<zipf, key_t>(benchmark_scale(), true) | rv::take(benchmark_scale() / thrnum))
     {
+        bool evil_fucking_bug_arrive{};
+        if (debug_set.contains(k))
+        {
+            evil_fucking_bug_arrive = true;
+        }
+        else debug_set.insert(k);
         ++iteration;
         benchmark_loop_body(cache, k, hits, misses);
+        if (evil_fucking_bug_arrive)
+            benchmark_loop_body(cache, k, hits, misses);
     }
 
     auto time_elapsed = toc(tp);
