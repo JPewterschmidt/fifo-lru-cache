7c358762d98a9f0b1d71e76b3591a55c93c7bc67
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-09-08 22:49:50 +0800

    update report

--- benchmark/main.cc
+++ benchmark/main.cc
@@ -16,9 +16,9 @@ using namespace nbtlru;
 
 int main(int argc, char** argv)
 {
-    different_dist();   
+    //different_dist();   
     //multi_threads_profiling(12, "naive", naive_lru_profiling_worker);
-    multi_threads_profiling(12, "queue", queue_lru_profiling_worker);
+    //multi_threads_profiling(12, "queue", queue_lru_profiling_worker);
     //multi_threads_profiling(12, "sampling", sampling_lru_profiling_worker);
 
     return 0;   
