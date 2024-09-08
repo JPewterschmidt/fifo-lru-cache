ca5cdd1d5683acf12f95cbe183edc2fc81a6b704
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-09-08 22:33:29 +0800

    update report and fifo_hybrid_lru

--- include/fifo_hybrid_lru.h
+++ include/fifo_hybrid_lru.h
@@ -43,6 +43,7 @@ private:
     
     using manage_block = ::std::atomic<::std::weak_ptr<lru_element>>;
     using lru_ele_sptr = ::std::shared_ptr<lru_element>;
+    using lru_ele_wptr = ::std::weak_ptr<lru_element>;
     using manage_block_sptr = ::std::shared_ptr<manage_block>;
 
 private:
@@ -51,22 +52,23 @@ private:
     ::std::size_t m_fifo_part_capacity{};
     ::std::size_t m_evict_thresh{};
     
-    using queue_type = moodycamel::ConcurrentQueue<manage_block_sptr>;
+    using lru_queue_type = moodycamel::ConcurrentQueue<manage_block_sptr>;
+    using fifo_queue_type = moodycamel::ConcurrentQueue<lru_ele_wptr>;
 
-    queue_type m_fifo_policy_q;
-    queue_type m_lru_policy_q;
+    fifo_queue_type m_fifo_policy_q;
+    lru_queue_type m_lru_policy_q;
     libcuckoo::cuckoohash_map<KeyType, lru_ele_sptr, Hash, KeyEq> m_hash;
 
 public:
-    fifo_hybrid_lru(size_t capacity, double fifo_part_ratio = 0.8, double evict_thresh_ratio = 0.95)
+    fifo_hybrid_lru(size_t capacity, double fifo_part_ratio = 0.7, double evict_thresh_ratio = 0.95)
         : m_capacity{ capacity }, 
           m_fifo_part_capacity{ static_cast<size_t>(fifo_part_ratio * m_capacity) }, 
           m_evict_thresh{ m_capacity * evict_thresh_ratio }
     {
         if (m_capacity < 1000)
             m_capacity = 1000;
-        if (m_fifo_part_capacity < 800)
-            m_fifo_part_capacity = 800;
+        if (m_fifo_part_capacity < m_capacity * fifo_part_ratio)
+            m_fifo_part_capacity = m_capacity * fifo_part_ratio;
     }
 
     result_type get(const KeyType& k)
@@ -77,10 +79,7 @@ public:
             return {};
         }
 
-        manage_block_sptr mb = ele->m_manage_block_ptr.exchange({}, ::std::memory_order_acq_rel);
-        mb->store({}, ::std::memory_order_release);
-        if (mb) promote(ele);
-        
+        promote(ele);
         return { ele, &ele->m_user_data.second };
     }
 
@@ -89,21 +88,25 @@ public:
     {
         evict_if_needed();
         auto ele = make_lru_element(k, ::std::forward<Args>(args)...);
-        m_hash.update(k, ele);
-        auto mb = make_manage_block();
-        mb->store(ele, ::std::memory_order_relaxed);
-        m_fifo_policy_q.enqueue(::std::move(mb));
+        m_hash.upsert(k, [&] (auto& entry) { entry = ele; }, ele);
+        m_fifo_policy_q.enqueue(ele);
+        m_size.fetch_add(1, ::std::memory_order_relaxed);
         balance();
         return { ele, &ele->m_user_data.second };
     }
 
-    void erase(const KeyType& k) { m_hash.erase(k); }
+    void erase(const KeyType& k) 
+    { 
+        m_hash.erase(k); 
+        m_size.fetch_sub(1, ::std::memory_order_relaxed);
+    }
 
     result_type get_then_erase(const KeyType& k)
     {
         lru_ele_sptr ele;
-        m_hash.uprase(k, [&ele](auto& entry) { 
+        m_hash.uprase(k, [&ele, this](auto& entry) { 
             ele = ::std::move(entry);
+            m_size.fetch_sub(1, ::std::memory_order_relaxed);
             return false;
         });
         return { ele, &ele->m_user_data.second };
@@ -116,8 +119,8 @@ public:
 
     void reset()
     {
-        m_fifo_policy_q = queue_type();
-        m_lru_policy_q = queue_type();
+        m_fifo_policy_q = fifo_queue_type();
+        m_lru_policy_q = lru_queue_type();
         m_hash.clear();
         m_size.store(0, ::std::memory_order_relaxed);
     }
@@ -125,14 +128,19 @@ public:
 private:
     void promote(lru_ele_sptr& ele) 
     {
-        auto new_mb = make_manage_block();
-        ele->m_manage_block_ptr.store(new_mb, ::std::memory_order_release);
-        m_fifo_policy_q.enqueue(::std::move(new_mb));
+        manage_block_sptr mb = ele->m_manage_block_ptr.exchange({}, ::std::memory_order_acq_rel);
+        if (mb) 
+        {
+            mb->store({}, ::std::memory_order_release);
+            m_fifo_policy_q.enqueue(ele);
+        }
     }
 
-    manage_block_sptr make_manage_block()
+    manage_block_sptr make_manage_block(lru_ele_sptr& ele)
     {
-        return ::std::make_shared<manage_block>();
+        auto result = ::std::make_shared<manage_block>();
+        result->store(ele, ::std::memory_order_relaxed);
+        return result;
     }
 
     template<typename... Args>
@@ -161,22 +169,20 @@ private:
                 break;
             }
         }
-
-        balance();
     }
 
     void balance()
     {
         while (m_fifo_policy_q.size_approx() >= fifo_part_capacity())
         {
-            manage_block_sptr mb;
-            if (!m_fifo_policy_q.try_dequeue(mb)) [[unlikely]]
+            lru_ele_wptr wele;
+            if (!m_fifo_policy_q.try_dequeue(wele)) [[unlikely]]
             {
                 return;
             }
-            auto ele = mb->load(::std::memory_order_acquire).lock();
-            if (ele)
+            if (auto ele = wele.lock(); ele)
             {
+                auto mb = make_manage_block(ele);
                 ele->m_manage_block_ptr.store(mb, ::std::memory_order_release);
                 m_lru_policy_q.enqueue(::std::move(mb));
                 return;
