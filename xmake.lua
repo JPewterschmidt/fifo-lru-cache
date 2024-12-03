54a1af509d5ba5d0a0b513b6fd7caa3fd61c31f9
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-12-03 21:36:45 +0800

    remove test target from main branch

--- xmake.lua
+++ xmake.lua
@@ -31,21 +31,3 @@ target("fifo-lru-cache")
         "libcuckoo/libcuckoo", 
         { public = true }
     )
-
-target("test")
-    set_kind("binary")
-    add_deps("fifo-lru-cache")
-    add_files(
-        "test/*.cc",
-        "smhasher/src/Murmur*.cpp", 
-        "libcuckoo/libcuckoo"
-    )
-    add_packages("gtest")
-    set_warnings("all", "error")
-    after_build(function (target)
-        os.execv(target:targetfile(), {"--gtest_color=yes"})
-        print("xmake: unittest complete.")
-    end)
-    on_run(function (target)
-        --nothing
-    end)
